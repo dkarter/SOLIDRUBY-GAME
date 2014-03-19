a8820364e69382f96fe74bca3f4c8997627cf15a
Dorian Karter <dkarter@gmail.com>
2014-03-19 00:55:20 -0700

    describe effect complete

--- spec/lib/attack_action_spec.rb
+++ spec/lib/attack_action_spec.rb
@@ -3,8 +3,10 @@ require_relative "../../lib/attack_action"
 
 describe AttackAction do
 	let(:dicepool) { double("dicepool") }
-	let(:hero) { double("hero", strength: 3) }
+	let(:hero) { double("hero", strength: 3, gain_exp: nil, gain_gold: nil, damage: nil) }
 	let(:action) { AttackAction.new hero, dicepool }
+	let(:monster) { double("monster", toughness: 2, kill: nil, damage: nil) }
+
 	it "responds to activate message" do
 		expect(action).to respond_to(:activate)
 	end
@@ -15,10 +17,37 @@ describe AttackAction do
 
 	describe "activate" do
 	  it "makes strength check against target toughness" do
-	  	monster = double("monster", toughness: 2)
 	  	dicepool.should_receive(:skill_check).with(hero.strength, monster.toughness)
 	  	action.activate(monster)	
 	  end
 
+	end
+
+	describe "effect" do
+	  context 'success' do
+	  	before(:each) do
+	  	  dicepool.stub(:skill_check).and_return(true)
+	  	end
+	  	after(:each) do
+	  		action.activate(monster)  
+	  	end
+	  	it "kills monster" do
+	  		monster.should_receive(:kill)
+	  	end
+	  	it "rewards owner with exp" do
+	  		hero.should_receive(:gain_exp)
+	  	end
+
+	  	it "rewards owner with gold" do
+	  		hero.should_receive(:gain_gold)
+	  	end
+	  end
+	  context 'failure' do
+	  	it "damages owner" do
+	  		dicepool.stub(:skill_check).and_return(false)
+	  		hero.should_receive(:damage).with(monster.damage)
+	  		action.activate(monster)
+	  	end
+	  end
 	end
 end
