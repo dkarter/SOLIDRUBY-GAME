61454283b0117943e094c448ed68278577a30eb2
Dorian Karter <dkarter@gmail.com>
2014-03-19 02:00:00 -0700

    completed specing and implementing effect (success and failure)

--- spec/lib/flee_action_spec.rb
+++ spec/lib/flee_action_spec.rb
@@ -3,7 +3,7 @@ require_relative "../../lib/flee_action"
 
 describe FleeAction do
 	let(:dicepool) { double("dicepool") }
-	let(:hero) { double("hero", strength: 3, gain_exp: nil, gain_gold: nil, damage: nil, stealth: 5) }
+	let(:hero) { double("hero", strength: 3, gain_exp: nil, gain_gold: nil, damage: nil, stealth: 5, flee: nil) }
 	let(:action) { FleeAction.new hero, dicepool }
 	let(:monster) { double("monster", toughness: 2, kill: nil, damage: nil, notice: 3) }
 
@@ -25,10 +25,18 @@ describe FleeAction do
 
 	describe "effect" do
 	  context "success" do
-	  	
+	  	it "sends flee message to owner" do
+	  		dicepool.stub(:skill_check).and_return(true)
+	  		hero.should_receive(:flee)
+	  		action.activate(monster)
+	  	end
 	  end
 	  context "failure" do
-	  	
+	  	it "deals damage to the owner" do
+	  		dicepool.stub(:skill_check).and_return(false)
+	  		hero.should_receive(:damage).with(monster.damage)
+	  		action.activate(monster)
+	  	end
 	  end
 	end
 end
